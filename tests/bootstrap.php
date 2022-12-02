0b84a0105240e25c611fbecc60d6ba6950462bfb
nojimage <nojimage@gmail.com>
2022-12-02 23:53:57 +0900

    update test/bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+
 /**
- * Test suite bootstrap for LoginAttempts.
+ * Test suite bootstrap for CakePHP Plugin.
  *
  * This function is used to find the location of CakePHP whether CakePHP
  * has been installed as a dependency of the plugin, or the plugin is itself
@@ -26,3 +29,9 @@ $here = __DIR__;
 
 chdir($root);
 require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
+
+Cache::clearAll();
+
+error_reporting(E_ALL);
+
+Configure::write('App.namespace', 'TestApp');
